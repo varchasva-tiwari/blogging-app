6e729e51dcea068ea3cd5f2cd96b0d45728ea408
varchasva <varchasva.tiwari@mountblue.tech>
2020-08-23 19:52:03 +0530

    Added missing classes to be loaded by Heroku

---
+++ src/main/java/com/mountblue/blogProject/controller/LoginController.java
@@ -0,0 +1,14 @@
+package com.mountblue.blogProject.controller;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+}
